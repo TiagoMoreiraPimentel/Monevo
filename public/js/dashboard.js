51dd8e6612fa1805d26ddf2b722a218439c08fad
Tiago Moreira Pimentel <65047406+TiagoMoreiraPimentel@users.noreply.github.com>
2025-06-19 22:28:52 -0300

    up

---
+++ public/js/dashboard.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
+
+  if (!usuario) {
+    alert("Acesso negado. Faça login.");
+    window.location.href = "../telas/login.html";
+    return;
+  }
+
+  const saudacao = document.getElementById("saudacao");
+  saudacao.textContent = `Olá, ${usuario.nome} (${usuario.nivel_acesso})`;
+
+  const botaoAdmin = document.getElementById("admin-only");
+  if (usuario.nivel_acesso !== "ADMINISTRADOR") {
+    botaoAdmin.classList.add("hidden");
+  }
+});
