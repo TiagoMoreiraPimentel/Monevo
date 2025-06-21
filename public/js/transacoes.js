300479b9b36083a1b6b249ff03742130f68824d5
Tiago Moreira Pimentel <65047406+TiagoMoreiraPimentel@users.noreply.github.com>
2025-06-21 01:29:32 -0300

    up

--- public/js/transacoes.js
+++ public/js/transacoes.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
       id_conta: idConta,
       tipo,
       valor,
-      data_transacao: new Date(dataBruta).toISOString(),  // formato ISO compatível
+      data_transacao: `${dataBruta}T00:00:00`,
       categoria,
       descricao
     };
