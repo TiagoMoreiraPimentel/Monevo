a7ba2181a7cbafeac9613697966e59ea5854a5f9
Tiago Moreira Pimentel <65047406+TiagoMoreiraPimentel@users.noreply.github.com>
2025-06-21 23:51:09 -0300

    up

--- public/js/transacoes.js
+++ public/js/transacoes.js
@@ -167,10 +167,10 @@ window.excluirTransacao = async function (id, idUsuario) {
   const confirmar = confirm("Deseja excluir esta transação?");
   if (!confirmar) return;
 
-  try {
-    const res = await fetch(`/api/transacoes/${id}`, {
+  const res = await fetch(`/api/transacoes?id=${id}`, {
       method: "DELETE"
     });
+
 
     if (res.ok) {
       mostrarMensagem("Transação excluída.");
