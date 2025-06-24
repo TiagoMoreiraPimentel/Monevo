943416a5d8bfe4e2df9c43aea717d3ebe205379f
Tiago Moreira Pimentel <65047406+TiagoMoreiraPimentel@users.noreply.github.com>
2025-06-24 20:46:24 -0300

    up

--- public/js/transacoes.js
+++ public/js/transacoes.js
@@ -1,3 +1,5 @@
+const contasPorId = {};
+
 document.addEventListener("DOMContentLoaded", () => {
   const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
   if (!usuario) {
@@ -10,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const tagContainer = document.getElementById("container-tag");
   const selectTag = document.getElementById("tag-distribuicao");
   const selectConta = document.getElementById("conta");
-  const contasPorId = {};
 
   document.getElementById("btn-voltar").addEventListener("click", () => {
     window.location.href = "/telas/dashboard.html";
