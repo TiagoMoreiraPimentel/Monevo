ab66cca91e244672c39a9f8664404ef9433f4cab
Tiago Moreira Pimentel <65047406+TiagoMoreiraPimentel@users.noreply.github.com>
2025-06-25 18:08:03 -0300

    up

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,16 +1,14 @@
-fetch('../telas/menu.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('menu-container').innerHTML = html;
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("../telas/menu.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("menu-container").innerHTML = html;
+    });
+});
 
-    // Dispara um evento para avisar que o menu foi carregado
-    document.dispatchEvent(new Event('menuCarregado'));
-  });
-
-// Função de alternar o menu lateral
 function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
+  const sidebar = document.getElementById("menuSidebar");
   if (sidebar) {
-    sidebar.classList.toggle('expanded');
+    sidebar.classList.toggle("expanded");
   }
 }
