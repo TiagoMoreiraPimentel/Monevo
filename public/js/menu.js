4c140abf09d8d151ad42593310f25f0363b57161
Tiago Moreira Pimentel <65047406+TiagoMoreiraPimentel@users.noreply.github.com>
2025-06-25 17:53:07 -0300

    up

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,10 +1,14 @@
-fetch('../telas/menu.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('menu-container').innerHTML = html;
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("../telas/menu.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("menu-container").innerHTML = html;
+    });
+});
 
 function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
-  sidebar.classList.toggle('hidden');
+  const sidebar = document.getElementById("menuSidebar");
+  if (sidebar) {
+    sidebar.classList.toggle("expanded");
+  }
 }
